a79baae5f07518e7903f8c7ca02fb6128aa55694
Brook Heisler <redattack34@gmail.com>
2015-12-22 18:24:24 -0600

    Rework the main code a bit.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 mod rom;
 
+use std::env;
+use std::path::Path;
+
 fn main() {
-    println!("Hello World!");
+    let args = env::args();
+    let rom_name = args.skip(1).next().expect("No ROM file provided.");
+    let path = Path::new(&rom_name);
+    let rom = rom::Rom::read(&path);
 }
