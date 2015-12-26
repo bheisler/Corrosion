7362f47ff1ab0e517c39fa90fd8d216b29964292
Brook Heisler <brookheisler@gmail.com>
2015-12-26 08:49:54 -0600

    Implemented emulation of the NROM mapper.

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ mod mappers;
 
 use std::env;
 use std::path::Path;
+use mappers::Mapper;
 
 fn main() {
     let args = env::args();
     let rom_name = args.skip(1).next().expect("No ROM file provided.");
     let path = Path::new(&rom_name);
-    let mut rom = rom::Rom::read(&path).expect("Failed to read ROM File");
+    let rom = rom::Rom::read(&path).expect("Failed to read ROM File");
     let mapper = mappers::Mapper::new( rom.mapper() as u16, rom.prg_rom, rom.chr_rom, rom.prg_ram );
+    mapper.borrow();
 }
--- src/mappers/nrom.rs
+++ src/mappers/nrom.rs
@@ -18,28 +18,93 @@ impl NROM {
 
 impl Mapper for NROM {
     fn prg_read(&self, idx: u16) -> u8 {
-        unimplemented!();
+        match idx {
+            0x6000 ... 0x7FFF => self.prg_ram[((idx - 0x6000) as usize % self.prg_ram.len())],
+            0x8000 ... 0xFFFF => self.prg_rom[((idx - 0x8000) as usize % self.prg_ram.len())],
+            _ => panic!("Invalid NES Memory address"),
+        }
     }
     
     fn prg_write(&mut self, idx: u16, val : u8) {
-        unimplemented!();
+        match idx {
+            0x6000 ... 0x7FFF => {
+                let idx = (idx - 0x6000) as usize % self.prg_ram.len();
+                self.prg_ram[idx] = val;
+            }
+            0x8000 ... 0xFFFF => (),//Do nothing
+            _ => panic!("Invalid NES Memory address"),
+        }
     }
     
     fn chr_read(&self, idx: u16) -> u8 {
-        unimplemented!();
+        self.chr_rom[idx as usize % self.chr_rom.len()]
     }
     
+    #[allow(unused_variables)]
     fn chr_write(&mut self, idx: u16, val : u8) {
-        unimplemented!();
+        //Do Nothing
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use ::mappers::Mapper;
     
     #[test]
     fn test_can_create_mapper_0() {
     	NROM::new( vec!(), vec!(), vec!() );
+    }
+    
+    fn create_test_mapper() -> NROM {
+        NROM::new( vec!(0u8; 0x4000), vec!(0u8; 0x4000), vec!(0u8; 0x1000) )
+    }
+    
+    #[test]
+    fn test_prg_ram_read_write() {
+    	let mut nrom = create_test_mapper();
+    	println!("{}", nrom.prg_ram.len() );
+    	
+    	nrom.prg_write( 0x6111, 15 );
+    	assert_eq!( nrom.prg_read( 0x6111 ), 15 );
+    	
+    	nrom.prg_write( 0x6112, 16 );
+    	assert_eq!( nrom.prg_read( 0x7112 ), 16 );
+    }
+    
+    #[test]
+    fn test_prg_rom_read() {
+        let prg_rom : Vec<_> = (0..0x4000)
+        	.map(|val| (val % 0xFF) as u8 )
+        	.collect();
+    	let mapper = NROM::new( prg_rom, vec!(0u8; 0x4000), vec!(0u8; 0x1000) );
+    	
+    	assert_eq!( mapper.prg_read(0x8111), mapper.prg_read(0xC111));
+    }
+    
+    #[test]
+    fn test_prg_rom_write() {
+    	let mut mapper = create_test_mapper();
+    	
+    	mapper.prg_write(0x8612, 15);
+    	assert_eq!( mapper.prg_read(0x8612), 0);
+    }
+    
+    #[test]
+    fn test_chr_rom_read() {
+        let chr_rom : Vec<_> = (0..0x2000)
+        	.map(|val| (val % 0xFF) as u8 )
+        	.collect();
+    	let mapper = NROM::new( vec!(0u8; 0x4000), chr_rom, vec!(0u8; 0x1000) );
+    	
+    	assert_eq!( mapper.prg_read(0x8111), mapper.prg_read(0xC111));
+    }
+    
+    #[test]
+    fn test_chr_rom_write() {
+    	let mut mapper = create_test_mapper();
+    	
+    	mapper.chr_write(0x1612, 15);
+    	assert_eq!( mapper.chr_read(0x1612), 0);
     }
 }
