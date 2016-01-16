fc343e81284ccf93cce82673f9379aa677381002
Brook Heisler <redattack34@gmail.com>
2016-01-16 14:57:49 -0600

    Got background rendering working.

--- src/ppu.rs
+++ src/ppu.rs
@@ -72,7 +72,10 @@ impl MemSegment for PPUMemory {
                 let mut cart = self.cart.borrow_mut();
                 cart.chr_write(idx, val)
             }
-            0x2000...0x3EFF => self.vram[(idx % 0x800) as usize] = val,
+            0x2000...0x3EFF => {
+                let idx = ((idx - 0x2000) % 0x800) as usize;
+                self.vram[idx] = val;
+            },
             0x3F00...0x3FFF => {
                 let val = Color::from_bits_truncate(val);
                 match (idx - 0x3F00) as usize {
@@ -90,8 +93,8 @@ impl MemSegment for PPUMemory {
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 enum AddrByte {
-    First,
-    Second,
+    High,
+    Low,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
@@ -312,7 +315,7 @@ impl PPU {
                 ppuscroll: 0,
                 ppuaddr: 0,
                 dyn_latch: 0,
-                address_latch: AddrByte::First,
+                address_latch: AddrByte::High,
             },
             oam: [OAMEntry::zero(); 64],
             ppu_mem: PPUMemory::new(cart),
@@ -386,10 +389,21 @@ impl PPU {
     fn get_background_pixel(&mut self, screen_x: u16, screen_y: u16) -> Color {
         let x = screen_x + self.reg.scroll_x() as u16;
         let y = screen_y + self.reg.scroll_y() as u16;
+        
+        let color_id = self.get_color_id(x, y);
+        let palette_id = self.get_palette_id(x, y);
+        
+        self.read_palette(palette_id, color_id)
+    }
+    
+    fn get_color_id(&mut self, x: u16, y: u16) -> u8 {
         let nametable_addr = self.get_nametable_addr( x, y );
         let tile_idx = self.ppu_mem.read(nametable_addr);
         
-        Color::from_bits_truncate(tile_idx)
+        let tile_table = self.reg.ppuctrl.background_table();
+        let pattern = self.read_tile_pattern( tile_idx, y & 0x07, tile_table );
+        
+        self.get_color_in_pattern(pattern, x as u32 & 0x07)
     }
     
     fn get_nametable_addr(&self, px_x: u16, px_y: u16) -> u16 {
@@ -398,6 +412,55 @@ impl PPU {
         let y = px_y / 8;
         let result = self.reg.ppuctrl.nametable_addr() + y * NAMETABLE_WIDTH as u16 + x;
         result
+    }
+    
+    fn read_tile_pattern(&mut self, tile_id: u8, fine_y_scroll: u16, tile_table: u16 ) -> (u8, u8) {
+        let lo_addr = self.get_tile_addr( tile_id, 0, fine_y_scroll, tile_table );
+        let hi_addr = self.get_tile_addr( tile_id, 8, fine_y_scroll, tile_table );
+        ( self.ppu_mem.read(lo_addr), self.ppu_mem.read(hi_addr) )
+    }
+    
+    fn get_tile_addr(&self, tile_id: u8, plane: u8, fine_y_scroll: u16, tile_table: u16) -> u16 {
+        let mut tile_addr = 0u16;
+        tile_addr |= fine_y_scroll;
+        tile_addr |= plane as u16; //Plane must be 0 for low or 8 for high
+        tile_addr |= (tile_id as u16) << 4;
+        tile_addr |= tile_table; //Table must be 0x0000 or 0x1000
+        tile_addr
+    }
+    
+    fn get_color_in_pattern(&self, pattern: (u8, u8), fine_x: u32) -> u8 {
+        let (lo, hi) = pattern;
+        let shift = 0x07 - fine_x;
+        let color_id_lo = lo.wrapping_shr(shift) & 0x01;
+        let color_id_hi = (hi.wrapping_shr(shift) & 0x01) << 1;
+        color_id_lo | color_id_hi
+    }
+    
+    fn get_palette_id(&mut self, x: u16, y: u16) -> u8 {
+        let attribute_addr = self.get_attribute_addr(x, y);
+        let attribute_byte = self.ppu_mem.read(attribute_addr);
+        self.get_palette_from_attribute(attribute_byte, x, y)
+    }
+    
+    fn get_attribute_addr(&self, x: u16, y: u16) -> u16 {
+        let x = x / 32;
+        let y = y / 32;
+        let attr_table = self.reg.ppuctrl.nametable_addr() + 0x03C0;
+        attr_table + (y * 8) + x
+    }
+    
+    fn get_palette_from_attribute(&self, attr: u8, x: u16, y: u16) -> u8 {
+        let mut at = attr;
+        if y & 0x10 != 0 { at >>= 4 }
+        if x & 0x10 != 0 { at >>= 2 }
+        at & 0x03
+    }
+    
+    fn read_palette(&mut self, palette_id: u8, color_id : u8 ) -> Color {
+        let offset = (palette_id << 2) | color_id;
+        let bits = self.ppu_mem.read(0x3F00 + offset as u16);
+        Color::from_bits_truncate(bits)
     }
     
     fn start_vblank(&mut self) -> bool {
@@ -425,14 +488,15 @@ impl PPU {
 
 fn write_addr_byte(latch: &mut AddrByte, target: &mut u16, val: u8) {
     match *latch {
-        AddrByte::First => {
+        AddrByte::High => {
             *target = (*target & 0x00FF) | ((val as u16) << 8);
+            *latch = AddrByte::Low;
         }
-        AddrByte::Second => {
+        AddrByte::Low => {
             *target = (*target & 0xFF00) | ((val as u16) << 0);
+            *latch = AddrByte::High;
         }
     }
-    *latch = AddrByte::Second;
 }
 
 impl MemSegment for PPU {
@@ -441,7 +505,7 @@ impl MemSegment for PPU {
             0x0000 => self.reg.dyn_latch,
             0x0001 => self.reg.dyn_latch,
             0x0002 => {
-                self.reg.address_latch = AddrByte::First;
+                self.reg.address_latch = AddrByte::High;
                 let res = self.reg.ppustat.bits | (self.reg.dyn_latch & 0b0001_1111);
                 self.reg.ppustat.remove( VBLANK );
                 res
@@ -522,7 +586,7 @@ mod tests {
         assert_eq!(getter(&ppu), 0xDEAD);
         ppu.write(idx, 0xED);
         assert_eq!(getter(&ppu), 0xDEED);
-        ppu.reg.address_latch = AddrByte::First;
+        ppu.reg.address_latch = AddrByte::High;
         ppu.write(idx, 0xAD);
         assert_eq!(getter(&ppu), 0xADED);
     }
@@ -601,9 +665,9 @@ mod tests {
     #[test]
     fn reading_ppustat_clears_addr_latch() {
         let mut ppu = create_test_ppu();
-        ppu.reg.address_latch = AddrByte::Second;
+        ppu.reg.address_latch = AddrByte::Low;
         ppu.read(0x2002);
-        assert_eq!(ppu.reg.address_latch, AddrByte::First);
+        assert_eq!(ppu.reg.address_latch, AddrByte::High);
     }
 
     #[test]
