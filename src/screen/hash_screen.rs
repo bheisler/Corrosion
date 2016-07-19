48b33c1aca53af96d3f63b183412af01ede9b895
Brook Heisler <redattack34@gmail.com>
2016-07-18 19:45:36 -0600

    Add hash-verifying screen impl.

--- src/screen/hash_screen.rs
+++ src/screen/hash_screen.rs
@@ -2,27 +2,65 @@ extern crate sha1;
 
 use ppu::{Color, SCREEN_BUFFER_SIZE};
 use screen::Screen;
-use sha1::Sha1;
+use self::sha1::{Digest, Sha1};
+use std::collections::HashMap;
 
-pub struct HashScreen {
-    frames: u64,
+fn hash_screen(buf: &[Color; SCREEN_BUFFER_SIZE]) -> Digest {
+    let newbuf: Vec<u8> = buf.iter()
+        .map(|col: &Color| col.bits())
+        .collect();
+
+    let mut s = Sha1::new();
+    s.update(&newbuf);
+    s.digest()
 }
 
-impl Default for HashScreen {
-    fn default() -> HashScreen {
-        HashScreen { frames: 0 }
+pub struct HashPrinter {
+    frames: u32,
+
+    delegate: Box<Screen>,
+}
+
+impl HashPrinter {
+    fn new(delegate: Box<Screen>) -> HashPrinter {
+        HashPrinter {
+            frames: 0,
+            delegate: delegate,
+        }
     }
 }
 
-impl Screen for HashScreen {
+impl Screen for HashPrinter {
     fn draw(&mut self, buf: &[Color; SCREEN_BUFFER_SIZE]) {
-        let newbuf: Vec<u8> = buf.iter()
-            .map(|col: &Color| col.bits())
-            .collect();
+        println!("Frame: {}, Hash: {}",
+                 self.frames,
+                 hash_screen(buf).to_string());
+        self.frames += 1;
 
-        let mut s = Sha1::new();
-        s.update(&newbuf);
-        println!("Frame: {}, Hash: {}", self.frames, s.digest().to_string());
+        self.delegate.draw(buf);
+    }
+}
+
+pub struct HashVerifier {
+    hashes: HashMap<u32, &'static str>,
+    frames: u32,
+}
+
+impl HashVerifier {
+    fn new(hashes: HashMap<u32, &'static str>) -> HashVerifier {
+        HashVerifier {
+            frames: 0,
+            hashes: hashes,
+        }
+    }
+}
+
+impl Screen for HashVerifier {
+    fn draw(&mut self, buf: &[Color; SCREEN_BUFFER_SIZE]) {
+        if self.hashes.contains_key(&self.frames) {
+            assert_eq!(self.hashes.get(&self.frames).unwrap(),
+                       &hash_screen(buf).to_string());
+        }
         self.frames += 1;
     }
 }
