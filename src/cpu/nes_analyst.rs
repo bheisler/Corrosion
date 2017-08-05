5389b0a1a3120d2c8a8b673c9e9e7349d06418b3
Brook Heisler <redattack34@gmail.com>
2017-08-05 08:31:29 -0600

    Track which instructions set/use which flags.

--- src/cpu/nes_analyst.rs
+++ src/cpu/nes_analyst.rs
@@ -11,21 +11,38 @@ pub struct Analyst<'a> {
     furthest_branch: u16,
     found_exit_point: bool,
 
+    pub last_sign_flag_set: u16,
+    pub last_overflow_flag_set: u16,
+    pub last_zero_flag_set: u16,
+    pub last_carry_flag_set: u16,
+
     instructions: FnvHashMap<u16, InstructionAnalysis>,
 }
 
+#[derive(Debug, Clone)]
 pub struct InstructionAnalysis {
     pub is_branch_target: bool,
+
+    pub sign_flag_used: bool,
+    pub overflow_flag_used: bool,
+    pub zero_flag_used: bool,
+    pub carry_flag_used: bool,
 }
 
 impl Default for InstructionAnalysis {
     fn default() -> InstructionAnalysis {
         InstructionAnalysis {
             is_branch_target: false,
+
+            sign_flag_used: false,
+            overflow_flag_used: false,
+            zero_flag_used: false,
+            carry_flag_used: false,
         }
     }
 }
 
+#[derive(Debug)]
 pub struct BlockAnalysis {
     pub entry_point: u16,
     pub exit_point: u16,
@@ -42,6 +59,11 @@ impl<'a> Analyst<'a> {
             cpu: cpu,
             furthest_branch: 0,
             found_exit_point: false,
+
+            last_sign_flag_set: 0,
+            last_carry_flag_set: 0,
+            last_zero_flag_set: 0,
+            last_overflow_flag_set: 0,
 
             instructions: FnvHashMap::default(),
         }
@@ -142,51 +164,139 @@ impl<'a> Analyst<'a> {
     fn sta(&mut self, _: u8) {}
 
     // Loads
-    fn ldx(&mut self, _: u8) {}
-    fn lda(&mut self, _: u8) {}
-    fn ldy(&mut self, _: u8) {}
+    fn ldx(&mut self, _: u8) {
+        self.sign_set();
+        self.zero_set();
+    }
+    fn lda(&mut self, _: u8) {
+        self.sign_set();
+        self.zero_set();
+    }
+    fn ldy(&mut self, _: u8) {
+        self.sign_set();
+        self.zero_set();
+    }
 
     // Logic/Math Ops
-    fn bit(&mut self, _: u8) {}
-    fn and(&mut self, _: u8) {}
-    fn ora(&mut self, _: u8) {}
-    fn eor(&mut self, _: u8) {}
-    fn adc(&mut self, _: u8) {}
-    fn sbc(&mut self, _: u8) {}
-    fn cmp(&mut self, _: u8) {}
-    fn cpx(&mut self, _: u8) {}
-    fn cpy(&mut self, _: u8) {}
-    fn inc(&mut self, _: u8) {}
-    fn iny(&mut self) {}
-    fn inx(&mut self) {}
-    fn dec(&mut self, _: u8) {}
-    fn dey(&mut self) {}
-    fn dex(&mut self) {}
-    fn lsr(&mut self, _: u8) {}
-    fn asl(&mut self, _: u8) {}
-    fn ror(&mut self, _: u8) {}
-    fn rol(&mut self, _: u8) {}
+    fn bit(&mut self, _: u8) {
+        self.sign_set();
+        self.carry_set();
+        self.zero_set();
+    }
+    fn and(&mut self, _: u8) {
+        self.sign_set();
+        self.zero_set();
+    }
+    fn ora(&mut self, _: u8) {
+        self.sign_set();
+        self.zero_set();
+    }
+    fn eor(&mut self, _: u8) {
+        self.sign_set();
+        self.zero_set();
+    }
+    fn adc(&mut self, _: u8) {
+        self.carry_used();
+        self.carry_set();
+        self.sign_set();
+        self.zero_set();
+        self.overflow_set();
+    }
+    fn sbc(&mut self, _: u8) {
+        self.carry_used();
+        self.carry_set();
+        self.sign_set();
+        self.zero_set();
+        self.overflow_set();
+    }
+    fn cmp(&mut self, _: u8) {
+        self.zero_set();
+        self.sign_set();
+        self.carry_set();
+    }
+    fn cpx(&mut self, _: u8) {
+        self.zero_set();
+        self.sign_set();
+        self.carry_set();
+    }
+    fn cpy(&mut self, _: u8) {
+        self.zero_set();
+        self.sign_set();
+        self.carry_set();
+    }
+    fn inc(&mut self, _: u8) {
+        self.zero_set();
+        self.sign_set();
+    }
+    fn iny(&mut self) {
+        self.zero_set();
+        self.sign_set();
+    }
+    fn inx(&mut self) {
+        self.zero_set();
+        self.sign_set();
+    }
+    fn dec(&mut self, _: u8) {
+        self.zero_set();
+        self.sign_set();
+    }
+    fn dey(&mut self) {
+        self.zero_set();
+        self.sign_set();
+    }
+    fn dex(&mut self) {
+        self.zero_set();
+        self.sign_set();
+    }
+    fn lsr(&mut self, _: u8) {
+        self.zero_set();
+        self.sign_set();
+        self.carry_set();
+    }
+    fn asl(&mut self, _: u8) {
+        self.zero_set();
+        self.sign_set();
+        self.carry_set();
+    }
+    fn ror(&mut self, _: u8) {
+        self.carry_used();
+        self.carry_set();
+        self.sign_set();
+        self.zero_set();
+    }
+    fn rol(&mut self, _: u8) {
+        self.carry_used();
+        self.carry_set();
+        self.sign_set();
+        self.zero_set();
+    }
 
     // Jumps
     fn jmp(&mut self) {
+        self.all_used();
         self.read_w_incr_pc();
         self.end_function();
     }
     fn jmpi(&mut self) {
+        self.all_used();
         self.read_w_incr_pc();
         self.end_function();
     }
     fn jsr(&mut self) {
+        self.all_used();
         self.read_w_incr_pc();
         self.end_function();
     }
     fn rts(&mut self) {
+        self.all_used();
         self.end_function();
     }
     fn rti(&mut self) {
+        self.all_used();
         self.end_function();
     }
     fn brk(&mut self) {
+        self.all_used();
         self.end_function();
     }
 
@@ -200,27 +310,35 @@ impl<'a> Analyst<'a> {
 
     // Branches
     fn bcs(&mut self) {
+        self.all_used();
         self.branch()
     }
     fn bcc(&mut self) {
+        self.all_used();
         self.branch()
     }
     fn beq(&mut self) {
+        self.all_used();
         self.branch()
     }
     fn bne(&mut self) {
+        self.all_used();
         self.branch()
     }
     fn bvs(&mut self) {
+        self.all_used();
         self.branch()
     }
     fn bvc(&mut self) {
+        self.all_used();
         self.branch()
     }
     fn bmi(&mut self) {
+        self.all_used();
         self.branch()
     }
     fn bpl(&mut self) {
+        self.all_used();
         self.branch()
     }
 
@@ -234,37 +352,89 @@ impl<'a> Analyst<'a> {
     }
 
     // Stack
-    fn plp(&mut self) {}
-    fn php(&mut self) {}
-    fn pla(&mut self) {}
+    fn plp(&mut self) {
+        self.carry_set();
+        self.sign_set();
+        self.zero_set();
+        self.overflow_set();
+    }
+    fn php(&mut self) {
+        self.all_used()
+    }
+    fn pla(&mut self) {
+        self.sign_set();
+        self.zero_set();
+    }
     fn pha(&mut self) {}
 
     // Misc
     fn nop(&mut self) {}
-    fn sec(&mut self) {}
-    fn clc(&mut self) {}
+    fn sec(&mut self) {
+        self.carry_set();
+    }
+    fn clc(&mut self) {
+        self.carry_set();
+    }
     fn sei(&mut self) {}
     fn sed(&mut self) {}
     fn cld(&mut self) {}
-    fn clv(&mut self) {}
-    fn tax(&mut self) {}
-    fn tay(&mut self) {}
-    fn tsx(&mut self) {}
-    fn txa(&mut self) {}
+    fn clv(&mut self) {
+        self.overflow_set();
+    }
+    fn tax(&mut self) {
+        self.sign_set();
+        self.zero_set();
+    }
+    fn tay(&mut self) {
+        self.sign_set();
+        self.zero_set();
+    }
+    fn tsx(&mut self) {
+        self.sign_set();
+        self.zero_set();
+    }
+    fn txa(&mut self) {
+        self.sign_set();
+        self.zero_set();
+    }
     fn txs(&mut self) {}
-    fn tya(&mut self) {}
+    fn tya(&mut self) {
+        self.sign_set();
+        self.zero_set();
+    }
     fn cli(&mut self) {}
 
     // Unofficial instructions
     fn u_nop(&mut self, _: u8) {}
-    fn lax(&mut self, _: u8) {}
+    fn lax(&mut self, _: u8) {
+        self.lda(0);
+        self.ldx(0);
+    }
     fn sax(&mut self, _: u8) {}
-    fn dcp(&mut self, _: u8) {}
-    fn isc(&mut self, _: u8) {}
-    fn slo(&mut self, _: u8) {}
-    fn rla(&mut self, _: u8) {}
-    fn sre(&mut self, _: u8) {}
-    fn rra(&mut self, _: u8) {}
+    fn dcp(&mut self, _: u8) {
+        self.dec(0);
+        self.cmp(0);
+    }
+    fn isc(&mut self, _: u8) {
+        self.sbc(0);
+        self.inc(0);
+    }
+    fn slo(&mut self, _: u8) {
+        self.asl(0);
+        self.ora(0);
+    }
+    fn rla(&mut self, _: u8) {
+        self.rol(0);
+        self.and(0);
+    }
+    fn sre(&mut self, _: u8) {
+        self.lsr(0);
+        self.eor(0);
+    }
+    fn rra(&mut self, _: u8) {
+        self.adc(0);
+        self.ror(0);
+    }
     fn kil(&mut self) {
         self.end_function();
     }
@@ -300,5 +470,68 @@ impl<'a> Analyst<'a> {
             0x4000...0x401F => 0xFF,
             _ => self.cpu.read(idx),
         }
+    }
+
+    fn sign_set(&mut self) {
+        self.last_sign_flag_set = self.current_instruction;
+    }
+
+    fn overflow_set(&mut self) {
+        self.last_overflow_flag_set = self.current_instruction;
+    }
+
+    fn zero_set(&mut self) {
+        self.last_zero_flag_set = self.current_instruction;
+    }
+
+    fn carry_set(&mut self) {
+        self.last_carry_flag_set = self.current_instruction;
+    }
+
+    fn all_used(&mut self) {
+        self.sign_used();
+        self.overflow_used();
+        self.zero_used();
+        self.carry_used();
+    }
+
+    fn sign_used(&mut self) {
+        if self.last_sign_flag_set == 0 {
+            return;
+        }
+        self.instructions
+            .get_mut(&self.last_sign_flag_set)
+            .unwrap()
+            .sign_flag_used = true;
+    }
+
+    fn overflow_used(&mut self) {
+        if self.last_overflow_flag_set == 0 {
+            return;
+        }
+        self.instructions
+            .get_mut(&self.last_overflow_flag_set)
+            .unwrap()
+            .overflow_flag_used = true;
+    }
+
+    fn zero_used(&mut self) {
+        if self.last_zero_flag_set == 0 {
+            return;
+        }
+        self.instructions
+            .get_mut(&self.last_zero_flag_set)
+            .unwrap()
+            .zero_flag_used = true;
+    }
+
+    fn carry_used(&mut self) {
+        if self.last_carry_flag_set == 0 {
+            return;
+        }
+        self.instructions
+            .get_mut(&self.last_carry_flag_set)
+            .unwrap()
+            .carry_flag_used = true;
     }
 }
